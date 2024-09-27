3d5fe76950158fffd37b3d0f8610d6b31f8049b4
alena1805 <145384204+alena1805@users.noreply.github.com>
2024-09-27 12:25:39 +0500

    Update 1.2.cpp

--- 1/1.2.cpp
+++ 1/1.2.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
+#include <limits> // Для std::numeric_limits
 
+// Функция для сравнения двух чисел и формирования строки результата
+std::string makeDecision(int x, int y) {
+    if (x > y) {
+        return std::to_string(x) + " > " + std::to_string(y);
+    } else if (x < y) {
+        return std::to_string(x) + " < " + std::to_string(y);
+    } else {
+        return std::to_string(x) + " = " + std::to_string(y);
+    }
+}
+
+int main() {
+    int x, y;
+
+    // Запрашиваем ввод для x
+    std::cout << "Введите первое целое число (x): ";
+    while (true) {
+        std::cin >> x;
+        if (std::cin.fail()) {
+            std::cin.clear(); // Сбрасываем состояние потока
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаем входной буфер
+            std::cout << "Неверный ввод. Пожалуйста, введите целое число: ";
+        } else {
+            break; // Ввод корректен, выходим из цикла
+        }
+    }
+
+    // Запрашиваем ввод для y
+    std::cout << "Введите второе целое число (y): ";
+    while (true) {
+        std::cin >> y;
+        if (std::cin.fail()) {
+            std::cin.clear(); // Сбрасываем состояние потока
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаем входной буфер
+            std::cout << "Неверный ввод. Пожалуйста, введите целое число: ";
